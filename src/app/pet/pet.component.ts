148ba16ddf0d953288771edd550d40e733292a85
anhnt2989 <anhnt2989@gmail.com>
2019-08-02 15:01:04 +0700

    editing css

---
+++ src/app/pet/pet.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.css']
+})
+export class PetComponent implements OnInit {
+  petName = 'Bubble';
+  petImage = 'https://touchcinema.com/storage/phim-the-lion-king/phim-the-lion-king.jpg';
+
+  constructor() {
+  }
+
+  updateName(name) {
+    this.petName = name;
+  }
+
+  updateImage(image) {
+    this.petImage = image;
+  }
+
+  ngOnInit() {
+  }
+
+}
